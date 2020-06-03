b2434ec2355528ef8bc7b5e72351b293f0c8dea5
Keith Millar <keithmillar@hotmail.ca>
2020-06-03 04:04:35 +0000

    survey file. asks questions in terminal, you answer, and it builds you a profile

---
+++ survey.js
@@ -0,0 +1,23 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('What\'s your name? Nicknames are also acceptable :) ', (name) => {
+  rl.question('What\'s an activity you like doing? ', (activity) => {
+    rl.question('What do you listen to while doing that? ', (listen) => {
+      rl.question('Which meal is your favourite (eg: dinner, brunch, etc.) ', (meal) => {
+        rl.question('What\'s your favourite thing to eat for that meal? ', (food) => {
+          rl.question('Which sport is your absolute favourite? ', (sport) => {
+            rl.question('What is your superpower? In a few words, tell us what you are amazing at! ', (superpower) => {
+              console.log(`Hey my name is ${name}, and I like ${activity} and listening to ${listen}. My fav meal is ${meal} and more specifically, ${food}. I like ${sport} and if I could have any superpower I would want ${superpower}`);
+              rl.close();
+            });
+          });
+        });
+      });
+    });
+  });
+});
